024bd25894e9779e34411755316a5eac029d2b7d
Phillip Merritt <phillip3m@gmail.com>
2019-03-07 10:00:14 -0600

    Changed game files to only use the binary representation when passing the state to the NN. Added clone and randomize state function
    Signed-off-by: Phillip Merritt <phillip3m@gmail.com>

--- funcs.py
+++ funcs.py
@@ -101,8 +101,8 @@ def playMatches(player1, player2, EPISODES, logger, turns_until_tau0, memory = N
                 logger.info('action: %d', action)
                 for r in range(env.grid_shape[0]):
                     logger.info(['----' if x == 0 else '{0:.2f}'.format(np.round(x,2)) for x in pi[env.grid_shape[1]*r : (env.grid_shape[1]*r + env.grid_shape[1])]])
-                logger.info('MCTS perceived value for %s: %f', state.to_domino(action) ,np.round(MCTS_value,2))
-                logger.info('NN perceived value for %s: %f', state.to_domino(action) ,np.round(NN_value,2))
+                logger.info('MCTS perceived value for %s: %f', state.all_domino[action] ,np.round(MCTS_value,2))
+                logger.info('NN perceived value for %s: %f', state.all_domino[action] ,np.round(NN_value,2))
                 logger.info('====================')
 
             ### Do the action
